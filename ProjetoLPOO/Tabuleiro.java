2463d82ec4c14b2ea52ccad0c11fb4e384bc3163
nanda <fbp@ecomp.poli.br>
2020-10-14 16:33:37 -0300

    colocando td no package

--- ProjetoLPOO/Tabuleiro.java
+++ ProjetoLPOO/Tabuleiro.java
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream:Tabuleiro.java
-=======
-//ALUNOS: FERNANDA BARBOSA DE PINHO E JOSÉ ALBERTO OLIVEIRA MOURA FILHO
-package ProjetoLPOO;
-
-//abacate
->>>>>>> Stashed changes:ProjetoLPOO/Tabuleiro.java
 public class Tabuleiro {
     private final int[][] gameArray;
 

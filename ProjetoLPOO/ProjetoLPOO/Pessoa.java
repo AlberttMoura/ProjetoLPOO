49e1af1515ec40caa9a2e25fc15e40a3d60ef144
nanda <fbp@ecomp.poli.br>
2020-10-20 10:47:09 -0300

    funcionando...

--- ProjetoLPOO/ProjetoLPOO/Pessoa.java
+++ ProjetoLPOO/ProjetoLPOO/Pessoa.java
@@ -1,6 +1,12 @@
+package ProjetoLPOO;
 public class Pessoa {
     private String nome;
 
+    public Pessoa(){}
+    public Pessoa(String nome)
+    {
+        this.nome = nome;
+    }
     //GETTERS
     public String getNome(){
         return this.nome;

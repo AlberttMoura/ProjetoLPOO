88735b0d699468bd3444439027dbee44d0c46c27
Alberto Moura <56458241+Betinhool@users.noreply.github.com>
2020-10-28 13:28:39 -0300

    Tudo certo, nada errado

--- src/br/com/poli/melhorlig4/Tabuleiro.java
+++ src/br/com/poli/melhorlig4/Tabuleiro.java
@@ -53,49 +53,45 @@ public class Tabuleiro {
         for(int linha = 0; linha < gameArray.length; linha++){
             for(int coluna = 0; coluna < gameArray[0].length; coluna++){
                 //Horizontal
-                int contx = 1;
+                int contx = 0;
                 if(coluna < gameArray[0].length - 4) {
-                    for (int i = 1; i < 4; i++) {
-                        if (gameArray[linha][coluna + i] == 1) {
+                    for (int i = 0; i < 4; i++) {
+                        if (gameArray[linha][coluna + i] == jogadorAtual.getId()) {
                             contx++;
                         } else {
-                            contx = 0;
                             break;
                         }
                     }
                 }
                 //Vertical
-                int conty = 1;
-                if(linha  < gameArray.length - 4) {
-                    for (int i = 1; i < 4; i++) {
+                int conty = 0;
+                if(linha  <= gameArray.length - 4) {
+                    for (int i = 0; i < 4; i++) {
                         if (gameArray[linha + i][coluna] == jogadorAtual.getId()) {
                             conty++;
                         } else {
-                            conty = 0;
                             break;
                         }
                     }
                 }
                 //DiagonalSup
-                int contxySup = 1;
-                if(linha < gameArray.length - 4 && coluna < gameArray[0].length - 4) {
-                    for (int i = 1; i < 4; i++) {
+                int contxySup = 0;
+                if(linha <= gameArray.length - 4 && coluna <= gameArray[0].length - 4) {
+                    for (int i = 0; i < 4; i++) {
                         if (gameArray[linha + i][coluna + i] == jogadorAtual.getId()) {
                             contxySup++;
                         } else {
-                            contxySup = 0;
                             break;
                         }
                     }
                 }
                 //DiagonalInf
-                int contxyInf = 1;
-                if(linha > 4 && coluna < gameArray[0].length - 4) {
-                    for (int i = 1; i < 4; i++) {
+                int contxyInf = 0;
+                if(linha > 4 && coluna <= gameArray[0].length - 4) {
+                    for (int i = 0; i < 4; i++) {
                         if (gameArray[linha - i][coluna + i] == jogadorAtual.getId()) {
                             contxyInf++;
                         } else {
-                            contxyInf = 0;
                             break;
                         }
                     }
